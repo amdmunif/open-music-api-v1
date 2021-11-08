5b9807a30375f8d0b14981cc7226f1ae94dfe2ff
amdmunif <ahmad.munif02@gmail.com>
2021-11-08 21:01:30 +0700

    initial commit

---
+++ src/validator/songs/schema.js
@@ -0,0 +1,14 @@
+/* eslint-disable eol-last */
+/* eslint-disable indent */
+const Joi = require('joi');
+
+const SongPayloadSchema = Joi.object({
+    title: Joi.string().required(),
+    year: Joi.number().required(),
+    performer: Joi.string().required(),
+    genre: Joi.string().required(),
+    duration: Joi.number().required(),
+    body: Joi.string().required(),
+});
+
+module.exports = { SongPayloadSchema };
